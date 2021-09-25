e6f85735be254193df5d4bcf973eaeee2e27fae7
vinsonw <me@vinsonwei.com>
2021-09-25 15:11:19 +0800

    add clear_bin.py to clear binary files

---
+++ clear_bin.py
@@ -0,0 +1,15 @@
+#! /usr/bin/python3
+
+import mimetypes
+import os
+
+for root, dirs, files in os.walk(".", topdown=False):
+    for dir_name in dirs:
+        if dir_name.startswith('chapter') or dir_name == 'playground':
+            target_files = os.listdir(dir_name)
+            for target_file in target_files:
+                type_, encodeing = mimetypes.guess_type(target_file)
+                if type_ == None:
+                    os.remove(
+                        os.path.join('.', dir_name, target_file)
+                    )
