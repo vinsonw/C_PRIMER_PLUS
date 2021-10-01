630f466ddd3c48811f6bfd3a4ab2f08ab60a25dc
vinsonw <me@vinsonwei.com>
2021-10-01 22:41:53 +0800

    add one comment

--- playground/test_else_keyword.c
+++ playground/test_else_keyword.c
@@ -10,7 +10,7 @@ int main(void)
             printf("<2\n");
         else if (input < 5)
             printf("[2,5)\n");
-
+        // the final "else" is left out here to test if it's okay to ommit it -- tested okay
         printf("Done\n");
     }
 
