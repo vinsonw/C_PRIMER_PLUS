3a2b25cba86ee9bcf7f3cce869e43fc13b9d1fad
vinsonw <me@vinsonwei.com>
2021-10-01 22:09:21 +0800

    add test_else_keyword.c

---
+++ playground/test_else_keyword.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Please enter an integer: ");
+    int input;
+    while ( 1==scanf("%d", &input) )
+    {
+    if (input < 2)
+        printf("<2\n");
+    else if (input < 5)
+        printf("[2,5)\n");
+
+    printf("Done\n");
+    }
+
+    return 0;
+}
