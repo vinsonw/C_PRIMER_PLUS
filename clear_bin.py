9cdc62f6ae7acb6b21de695fea503079c24cca6d
vinsonw <me@vinsonwei.com>
2021-10-09 20:43:00 +0800

    refactor clear_bin.py
    allow calling to clear_bin.py from any directory

--- clear_bin.py
+++ clear_bin.py
@@ -3,16 +3,22 @@
 import mimetypes
 import os
 
-for root, dirs, files in os.walk(".", topdown=False):
+root_dir = os.path.dirname(
+    os.path.abspath(__file__)
+)
+
+print(root_dir)
+
+for root, dirs, files in os.walk(root_dir, topdown=False):
     for dir_name in dirs:
         if dir_name.startswith('chapter') or dir_name == 'playground':
-            target_files = os.listdir(dir_name)
+            target_files = os.listdir(os.path.join(root_dir,dir_name))
             for target_file in target_files:
                 type_, encodeing = mimetypes.guess_type(target_file)
                 if type_ == None:
                     os.remove(
-                        os.path.join('.', dir_name, target_file)
+                        os.path.join(root_dir, dir_name, target_file)
                     )
                     print('deleted: ', 
-                        os.path.join('.', dir_name, target_file)
+                        os.path.join(root_dir, dir_name, target_file)
                     )
