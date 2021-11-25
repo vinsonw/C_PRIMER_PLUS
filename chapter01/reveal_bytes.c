de96b38bd7dd14bb13b0363de2a422f39bab8947
vinsonw <me@vinsonwei.com>
2021-11-25 13:46:41 +0800

    reveal the size of pointer type
    Sssh! It's universally 8 bytes, not 8 bits.

--- chapter01/reveal_bytes.c
+++ chapter01/reveal_bytes.c
@@ -18,5 +18,19 @@ int main(void)
     printf("long double == %d bytes\n", sizeof (long double));
     printf(">>> And one byte == %d bits\n", __CHAR_BIT__);
 
+    printf("\nAbout pointers:\n");
+    printf("char * == %d bytes\n", sizeof (char *));
+    printf("unsigned char * == %d bytes\n", sizeof (unsigned char *));
+    printf("short * == %d bytes\n", sizeof (short *));
+    printf("unsigned short * == %d bytes\n", sizeof (unsigned short *));
+    printf("int * == %d bytes\n", sizeof (int *));
+    printf("unsigned int * == %d bytes\n", sizeof (unsigned int *));
+    printf("long * == %d bytes\n", sizeof (long *));
+    printf("unsigned long * == %d bytes\n", sizeof (unsigned long *));
+    printf("long long * == %d bytes\n", sizeof (long long *));
+    printf("unsigned long long * == %d bytes\n", sizeof (unsigned long long *));
+    printf("float * == %d bytes\n", sizeof (float *));
+    printf("double * == %d bytes\n", sizeof (double *));
+    printf("long double * == %d bytes\n", sizeof (long double *));
     return 0;
 }
