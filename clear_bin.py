5afcd73a7274b03302f66c243e24237e3409a1fd
vinsonw <me@vinsonwei.com>
2021-11-26 16:02:21 +0800

    update clear_bin.py
    I guess I am gonna keep doing this.

--- clear_bin.py
+++ clear_bin.py
@@ -5,15 +5,17 @@ import os
 
 def delete_binary_file(target_file, dir_name):
     isBinary = False
+    isSkipped = False
     type_, encodeing = mimetypes.guess_type(target_file)
     if type_ == None:
+        isBinary = True
         if  target_file.endswith(".dat"):
             print(f"Skipping {os.path.join(dir_name, target_file)}")
-            return isBinary
+            isSkipped = True
+            return isBinary, isSkipped
         os.remove( os.path.join(dir_name, target_file) )
         print( 'deleted: ', os.path.join(dir_name, target_file) )
-        isBinary = True
-    return isBinary
+    return isBinary, isSkipped
 
 
 if __name__ == '__main__':
@@ -21,12 +23,29 @@ if __name__ == '__main__':
     print('working from:', root_dir)
     existsBinary = False
 
+    deleted = 0
+    skipped = 0
+
     for root, dirs, files in os.walk(root_dir, topdown=True):
         # print(f">>> {root}, {dirs}, {files}")
         if '.git' in root or '.vscode' in root:
             continue # skip hidden directories
         for file in files:
-            existsBinary = existsBinary or delete_binary_file(file, root)
 
+            # the following statement has a bug in it.
+            # existsBinary = existsBinary or delete_binary_file(file, root)
+
+            # this one will work, first operand will always be executed
+            # existsBinary = delete_binary_file(file, root) or existsBinary
+            
+            # refactory
+            binary_result, skip_result = delete_binary_file(file, root)
+            if binary_result and not skip_result:
+                deleted += 1
+            if skip_result:
+                skipped += 1
+            existsBinary = existsBinary or binary_result # not relying on binary_result to be executed
+
+    print(f'{deleted} deleted, {skipped} skipped.')
     if not existsBinary:
-        print('No binaries to delete.')
+        print('No binaries exist.')
